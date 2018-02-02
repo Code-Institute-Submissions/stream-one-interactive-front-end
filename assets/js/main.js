6b361868b391173128b123a01a6cc0d7ec083e0a
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-02-02 18:12:11 +0000

    js work

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,13 @@
 // ------------- Need to set location with user input ------------//
 
 var searchLocation = "london";
-var url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+searchLocation+'&key=AIzaSyD-CXRwTcTgC8tAAbiYZ6T4BWGD9FK9uCs';
+var geoUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='+searchLocation+'&key=AIzaSyD-CXRwTcTgC8tAAbiYZ6T4BWGD9FK9uCs';
 
-function getData(url, locationData) { //argument of cb passed to function
+function getLocation(geoUrl, locationData) { //argument of cb passed to function
     
     var xhr = new XMLHttpRequest(); // new variable set to a new instance of XMLHttpRequest Object
     
-    xhr.open("GET", url); // .open method called to set the argument of the api URL
+    xhr.open("GET", geoUrl); // .open method called to set the argument of the api URL
     xhr.send(); // request posted
     
     xhr.onreadystatechange = function() {
@@ -22,16 +22,51 @@ function getData(url, locationData) { //argument of cb passed to function
     
 }
 
-getData(url, function(data) {
+
+
+// invoke getData and parse jSON to extract Lat/Long to set search params and position map //
+
+
+
+getLocation(geoUrl, function(data) {
     
     var geoData = data.results[0].geometry.location;
-    var lat = geoData.lat;
-    var long = geoData.lng;
+    
+    var lat = geoData.lat.toString();
+    var long = geoData.lng.toString();
 
-    console.log(long);
+    console.log(lat,long);
+    console.log(typeof(lat,long));
 
- 
-    console.log(geoData);
+
+});
+
+
+var type = "vet";
+var x = "51.5073509";
+var y = "-0.1277583";
+var nearbyUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+x+','+y+'&radius=500&type='+type+'key=AIzaSyD-CXRwTcTgC8tAAbiYZ6T4BWGD9FK9uCs';
+
+function getNearby(nearbyUrl, nearbyData) {
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("GET", nearbyUrl); 
+    xhr.send(); 
+    
+    xhr.onreadystatechange = function() {
+       
+        if (this.readyState == 4 && this.status == 200) {  
+            nearbyData(JSON.parse(this.responseText));
+            
+        }
+    };
+    
+}
+
+getLocation(nearbyUrl, function(nearby) {
+        
+    console.log(nearby);
 
 
 });
